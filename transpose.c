78a93a080a6b9880d877cdd07aba19c9998682b3
khushichadha100 <122161486+khushichadha100@users.noreply.github.com>
2023-01-07 19:51:03 +0530

    Create transpose.c

---
+++ transpose.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int array[5][5],transpose[5][5],rows,cols;
+    printf("Enter number of rows of the matrix: ");
+    scanf("%d",&rows);
+    printf("Enter number of columns of the matrix: ");
+    scanf("%d",&cols);
+    printf("Input the elements of the matrix:\n");
+     
